c9dc5a84f549fd4f69842693be6e3391c375f3fd
石博文 <sbw@sbw.so>
2017-02-03 14:22:13 +0800

    support rules file

--- src/main.rs
+++ src/main.rs
@@ -24,39 +24,83 @@ impl Profile {
         profile
     }
 
-    pub fn read_pub_dns_list<T: Read>(&mut self, r: T) {
+    // pub fn read_pub_dns_list<T: Read>(&mut self, r: T) {
+
+    //     let rdr = BufReader::new(r);
+
+    //     self.public_dns_list.clear();
+    //     for line in rdr.lines() {
+    //         match line.unwrap().parse::<IpAddr>() {
+    //             Ok(addr) => self.public_dns_list.push(addr),
+    //             _ => continue,
+    //         }
+    //     }
+    // }
+
+    // pub fn read_spec_dns_list<T: Read>(&mut self, r: T) {
+
+    //     let rdr = BufReader::new(r);
+
+    //     self.spec_dns_list.clear();
+    //     for line in rdr.lines() {
+    //         match line.unwrap().parse::<IpAddr>() {
+    //             Ok(addr) => self.spec_dns_list.push(addr),
+    //             _ => continue,
+    //         }
+    //     }
+    // }
+
+    // pub fn read_spec_host_list<T: Read>(&mut self, r: T) {
+
+    //     let rdr = BufReader::new(r);
+
+    //     self.spec_hosts.clear();
+    //     for line in rdr.lines() {
+    //         self.spec_hosts.push(line.unwrap());
+    //     }
+    // }
+
+    pub fn read_rules<T: Read>(&mut self, r: T) {
 
         let rdr = BufReader::new(r);
+        let mut cursor = rdr.lines();
 
-        self.public_dns_list.clear();
-        for line in rdr.lines() {
-            match line.unwrap().parse::<IpAddr>() {
-                Ok(addr) => self.public_dns_list.push(addr),
-                _ => continue,
+        while let Some(Ok(line)) = cursor.next() {
+            if line.is_empty() {
+                break;
             }
-        }
-    }
 
-    pub fn read_spec_dns_list<T: Read>(&mut self, r: T) {
-
-        let rdr = BufReader::new(r);
-
-        self.spec_dns_list.clear();
-        for line in rdr.lines() {
-            match line.unwrap().parse::<IpAddr>() {
-                Ok(addr) => self.spec_dns_list.push(addr),
-                _ => continue,
+            if line.bytes().next() == Some(b'#') {
+                continue;
             }
+
+            self.public_dns_list.push(line.parse().unwrap());
+            // println!("{}", line);
         }
-    }
 
-    pub fn read_spec_host_list<T: Read>(&mut self, r: T) {
 
-        let rdr = BufReader::new(r);
+        while let Some(Ok(line)) = cursor.next() {
+            if line.is_empty() {
+                break;
+            }
 
-        self.spec_hosts.clear();
-        for line in rdr.lines() {
-            self.spec_hosts.push(line.unwrap());
+            if line.bytes().next() == Some(b'#') {
+                continue;
+            }
+
+            self.spec_dns_list.push(line.parse().unwrap());
+        }
+
+        while let Some(Ok(line)) = cursor.next() {
+            if line.is_empty() {
+                continue;
+            }
+
+            if line.bytes().next() == Some(b'#') {
+                continue;
+            }
+
+            self.spec_hosts.push(line);
         }
     }
 
@@ -96,6 +140,9 @@ impl Profile {
 
 fn main() {
 
+    let home_dir = std::env::home_dir().unwrap();
+    let default_list = format!("{}/.dnsmasq.rules", home_dir.display());
+
     let matches = App::new("dnsmasq-profile-generater")
         .author("sbw <sbw@sbw.so>")
         .version("0.0.1")
@@ -105,14 +152,32 @@ fn main() {
             .takes_value(true)
             .help("specificed output result file")
             .default_value("/etc/dnsmasq.conf"))
+        .arg(Arg::with_name("list")
+            .short("l")
+            .takes_value(true)
+            .help("rule list file")
+            .default_value(&default_list))
         .get_matches();
 
     let output = matches.value_of("output").unwrap();
+    let rules = matches.value_of("list").unwrap();
 
-    let pub_dns = b"8.8.8.8\n8.8.4.4\n";
-    let spec_dns = b"114.114.114.114\n115.115.115.115\n";
-    let spec_hosts = b"bilibili.com\nqq.com\n";
+    // let pub_dns = b"8.8.8.8\n8.8.4.4\n";
+    // let spec_dns = b"114.114.114.114\n115.115.115.115\n";
+    // let spec_hosts = b"bilibili.com\nqq.com\n";
 
+    let list_file = match OpenOptions::new().read(true).open(rules) {
+        Ok(file) => file,
+        Err(e) => panic!("Unable to open {}, {}", rules, e),
+    };
+
+    let mut profile = Profile::new();
+    // profile.read_pub_dns_list(&pub_dns[..]);
+    // profile.read_spec_dns_list(&spec_dns[..]);
+    // profile.read_spec_host_list(&spec_hosts[..]);
+    profile.read_rules(list_file);
+
+    // write config file
     let config_file = match OpenOptions::new()
         .read(true)
         .write(true)
@@ -122,10 +187,5 @@ fn main() {
         Ok(file) => file,
         Err(e) => panic!("Unable to open {}, {}", output, e),
     };
-
-    let mut profile = Profile::new();
-    profile.read_pub_dns_list(&pub_dns[..]);
-    profile.read_spec_dns_list(&spec_dns[..]);
-    profile.read_spec_host_list(&spec_hosts[..]);
     profile.write(config_file).unwrap();
 }
